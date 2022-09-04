f3d277d3c2fc15e46d2030622810d7dc15964a0e
ludfyrahman <ludfyr@gmail.com>
2022-09-04 10:51:05 +0700

    add color in map

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        
+        $this->call([
+            AdminSeeder::class,
+            UserSeeder::class,
+        ]);
+
     }
 }
